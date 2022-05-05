88839e2400361d81ed3a0b0ec904647dd44dc32e
CWMasters <95546410+CWMasters@users.noreply.github.com>
2022-05-04 17:09:25 -0700

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
   const [categories] = useState([
     {
       name: 'commercial',
@@ -23,13 +24,20 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
       <main>
-        <div>
-          <ContactForm></ContactForm>
+        {/* ternary operator. SHORTHAND condition*/}
+        {/* REACT FRAGMENTS <> </>  shorthand of <React.Fragment></React.Fragment> */}
+        {!contactSelected ? (
+          <>
           <Gallery currentCategory={currentCategory}></Gallery>
           <About></About>
-        </div>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )};
       </main>
     </div>
   );
